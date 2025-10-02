4a4c2f45786047fafa15c7f385aa5f068e7c2f3e
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-10-02 18:15:56 -0300

    feat: sphere rendering

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -1,53 +1,62 @@
 mod ray;
-// mod hitable;
-// mod material;
-// mod sphere;
-// mod camera;
 
-// use std::f32;
-// use nalgebra::Vector3;
-// use rand::Rng;
-// use rayon::prelude::*;
-// use crate::ray::Ray;
-// use crate::material::{Lambertian, Metal, Dielectric};
-// use crate::hitable::{Hitable, HitableList};
-// use crate::sphere::Sphere;
-// use crate::camera::Camera;
-
+use glam::DVec3;
+use indicatif::ProgressIterator;
 use itertools::Itertools;
 use std::{fmt::format, fs, io};
-use indicatif::ProgressIterator;
-use glam::DVec3;
 
+use crate::ray::Ray;
 
 const ASPECT_RADIO: f64 = 16.0 / 9.0;
 // Constantes para definir as dimensões da imagem e valor máximo de cor
-const IMAGE_WIDTH: u32 = 256;
-const IMAGE_HEIGTH: u32 = ((IMAGE_WIDTH as f64 / ASPECT_RADIO) as u32);
+const IMAGE_WIDTH: u32 = 400;
+const IMAGE_HEIGTH: u32 = if ((IMAGE_WIDTH as f64 / ASPECT_RADIO) as u32) < 1 {
+    1
+} else {
+    (IMAGE_WIDTH as f64 / ASPECT_RADIO) as u32
+}; //talvez reverter
 
+const VIEWPORT_HEIGTH: f64 = 2.0;
+const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGTH * (IMAGE_WIDTH as f64 / IMAGE_HEIGTH as f64);
 
+const FOCAL_LENGTH: f64 = 1.0;
+const CAMERA_CENTER: DVec3 = DVec3::ZERO;
+
+const VIEWPORT_U: DVec3 = DVec3::new(VIEWPORT_WIDTH, 0., 0.);
+const VIEWPORT_V: DVec3 = DVec3::new(0., -VIEWPORT_HEIGTH, 0.);
 
 const MAX_VALUE: u8 = 255;
 
-
-
-
-
 fn main() -> io::Result<()> {
+
+    let pixel_delta_u: DVec3 = VIEWPORT_U / IMAGE_WIDTH as f64;
+    let pixel_delta_v: DVec3 = VIEWPORT_V / IMAGE_HEIGTH as f64;
+
+    let viewport_upper_left: DVec3 = CAMERA_CENTER
+        - DVec3::new(0., 0., FOCAL_LENGTH)
+        - VIEWPORT_U / 2.
+        - VIEWPORT_V / 2.;
+    
+    let pixel100_loc: DVec3 = viewport_upper_left + 0.5 * (pixel_delta_u * pixel_delta_v);
+
     // Gera uma imagem gradiente usando o produto cartesiano de coordenadas y e x
     let pixels: String = (0..IMAGE_HEIGTH)
         .cartesian_product(0..IMAGE_WIDTH) // Cria todas as combinações possíveis de (y, x)
-        .progress_count(
-            IMAGE_HEIGTH as u64 * IMAGE_WIDTH as u64
-        ) 
+        .progress_count(IMAGE_HEIGTH as u64 * IMAGE_WIDTH as u64)
         .map(|(y, x)| {
-            // Calcula os valores RGB normalizados (0.0 a 1.0) baseados na posição do pixel
-            let red: f64 = x as f64 / (IMAGE_HEIGTH as f64 - 1.0);   // Gradiente horizontal (vermelho)
-            let green: f64 = y as f64 / (IMAGE_HEIGTH as f64 - 1.0); // Gradiente vertical (verde)
-            let blue: f64 = 0.0;                                      // Sem componente azul
             
+            let pixel_center: DVec3 = pixel100_loc + (x as f64 * pixel_delta_u) + (y as f64 * pixel_delta_v);
+            let ray_direction: DVec3 = pixel_center - CAMERA_CENTER;
+
+            let ray = Ray{
+                origin: CAMERA_CENTER,
+                direction: ray_direction,
+            };
+
+            let pixel_color = ray.color() * 255.0;
+
             // Converte os valores normalizados para a escala 0-255 e formata como string
-            format!("{} {} {}", red * 255.0, green * 255.0, blue * 255.0)
+            format!("{} {} {}", pixel_color.x, pixel_color.y, pixel_color.z)
         })
         // Agrupa os pixels em linhas (chunks) de acordo com a largura da imagem
         .chunks(IMAGE_WIDTH as usize)
@@ -55,10 +64,10 @@ fn main() -> io::Result<()> {
         // Junta cada linha de pixels com espaços e depois junta todas as linhas com quebras de linha
         .map(|chunk| chunk.into_iter().join(" "))
         .join("\n");
-    
+
     // Exibe os pixels no console (para debug)
     //println!("{}", pixels);
-    
+
     // Escreve a imagem no formato PPM (Portable Pixmap Format)
     // P3 = formato ASCII, seguido por largura, altura, valor máximo e dados dos pixels
     fs::write(
@@ -72,4 +81,15 @@ fn main() -> io::Result<()> {
         ),
     )?;
     Ok(())
+}
+
+
+fn hit_sphere(center: &DVec3, radius: f64, ray: &Ray) -> bool{
+    let distanceOriginCenter:DVec3 = ray.origin - *center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * distanceOriginCenter.dot(ray.direction);
+    let c = distanceOriginCenter.dot(distanceOriginCenter) - radius * radius;
+    let discriminant = b * b - 4. * a *c;
+    discriminant >= 0.
+
 }
--- raytracing/src/ray.rs
+++ raytracing/src/ray.rs
@@ -1,5 +1,7 @@
 use glam::DVec3;
 
+use crate::hit_sphere;
+
 // pub struct Ray {
 //     a: Vector3<f32>,
 //     b: Vector3<f32>
@@ -15,15 +17,27 @@ use glam::DVec3;
 //     pub fn point_at_parameter(&self, t: f32) -> Vector3<f32> { self.a + t * self.b }
 // }
 
-struct Ray{
-  origin: DVec3,
-  direction: DVec3
+pub struct Ray{
+  pub origin: DVec3,
+  pub direction: DVec3
 }
 
 impl Ray{
 
   fn sense(&self, time: f64) -> DVec3{
     self.origin + time * self.direction
+  }
+
+  pub fn color(&self) -> DVec3{
+
+    if hit_sphere(&DVec3::new(0., 0., -1.), 0.5, self){
+      return DVec3::new(1., 0., 0.);
+    };
+
+    let unit_direction: DVec3 = self.direction.normalize();
+
+    let alpha = 0.5 * (unit_direction.y + 1.0);
+    return (1.0 - alpha) * DVec3::new(1.0, 1.0, 1.0) + alpha * DVec3::new(0.5, 0.7, 1.0);
   }
 
 }
